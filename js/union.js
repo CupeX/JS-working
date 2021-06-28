474157a38d14841ccb6f7ab4c675b14d4c3fcf75
CupeX <dejan.dj@hotmail.com>
2021-06-28 13:24:20 +0200

    union update with recurse()

--- js/union.js
+++ js/union.js
@@ -1,6 +1,7 @@
 // union
 const arr1 = [1, 2, 3, [4, 5]];
-const arr2 = [100, 2, 1, 10, [4, [5]]];
+const arr2 = [100, 2, 1, 10, [4, [5, [[6]]]]];
+const arr3 = arr1.concat(arr2).flat(Infinity);
 
 const union = (a, b) => {
   const newArr = a.concat(b).flat(Infinity);
@@ -13,24 +14,45 @@ const union = (a, b) => {
     return a - b;
   });
 
-  console.log(uniqueArr);
+  // console.log(uniqueArr);
   return uniqueArr;
 };
 union(arr1, arr2);
 
 // union with reducer
-const arr3 = arr1.concat(arr2).flat(Infinity);
-const test = arr3.reduce((acc, cur) => {
-  if (!acc.includes(cur)) {
-    acc.push(cur);
-  }
-  return acc;
-}, []);
+const reduceFn = prop => {
+  const sorted = prop.reduce((acc, cur) => {
+    if (!acc.includes(cur)) {
+      acc.push(cur);
+    }
+    return acc;
+  }, []);
+  console.log('sorted', sorted);
+};
 
-console.log(
-  test.sort((a, b) => {
-    return a - b;
-  })
-);
+reduceFn(arr3);
+
+// union with recursion()
+const arr4 = arr1.concat(arr2);
+
+const flattener = prop => {
+  let flatArray = [];
+  let i, max_i;
+  for (i = 0, max_i = prop.length; i <= max_i; i++) {
+    if (typeof prop[i] == 'number') {
+      flatArray.push(prop[i]);
+    } else if (Array.isArray(prop[i])) {
+      const extract = prop[i];
+      flatArray = [...flatArray, ...extract];
+      sortedArr = [...flatArray];
+      console.log('flat', flatArray, 'sorted', sortedArr);
+      flattener(flatArray);
+    }
+  }
+  reduceFn(sortedArr);
+  return sortedArr;
+};
+
+flattener(arr4);
 
 module.exports = union;
